f3a3c404616e66b0cccf27405c9f116aefa1c552
Nanor Bright Suka <brightnanor19@gmail.com>
2026-02-21 15:33:42 +0000

    Added default test class

---
+++ src/test/java/com/backend/authsystem/AuthSystemApplicationTest.java
@@ -0,0 +1,15 @@
+package com.backend.authsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class AuthSystemApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // This test ensures that Spring Application Context starts correctly
+    }
+
+}
